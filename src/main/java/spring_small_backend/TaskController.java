0ec9ecb6f6f1d20c9db1126cd19fdfa581dec3e4
hernanbosqued <hernanbosqued@gmail.com>
2018-08-17 09:57:05 -0300

    restful

--- src/main/java/spring_small_backend/TaskController.java
+++ src/main/java/spring_small_backend/TaskController.java
@@ -1,38 +1,48 @@
 package spring_small_backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.concurrent.atomic.AtomicLong;
+import javax.validation.Valid;
 
 @RestController
 public class TaskController {
 
-    @Autowired
-    private TaskRepository repository;
+    private final TaskRepository repository;
 
-    //private final AtomicLong counter = new AtomicLong();
-    //@RequestMapping("/greeting")
-    //public Task greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-    //    return new Task(counter.incrementAndGet(), name,"description", true);
-    //}
-
-    @GetMapping(path="/add") // Map ONLY GET Requests
-    //public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
-    public @ResponseBody String addNewUser () {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-        Task n = new Task();
-        n.setDescription("lombok description");
-        n.setTitle("lombok title");
-        n.setDone(true);
-        repository.save(n);
-        return "Saved";
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path = "/tasks")
     public @ResponseBody
     Iterable<Task> getTasks() {
         return repository.findAll();
+    }
+
+    @PostMapping("/tasks")
+    public Task createNote(@Valid @RequestBody Task note) {
+        return repository.save(note);
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTask(@PathVariable(value = "id") long id) {
+        return repository.findById(id).orElseThrow(null);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task updateNote(@PathVariable(value = "id") long id, @Valid @RequestBody Task task) {
+        Task response = repository.findById(id).orElseThrow(null);
+        response.setTitle(task.title);
+        response.setDescription(task.description);
+        response.setDone(task.done);
+        return repository.save(response);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Task> deleteNote(@PathVariable(value = "id") long id) {
+        Task note = repository.findById(id).orElseThrow(null);
+        repository.delete(note);
+        return ResponseEntity.ok().build();
     }
 }
